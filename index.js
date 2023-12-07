e0ff2a1739c5108b535a912d9ed3933ad2098e4c
999Kit <98326962+999Kit@users.noreply.github.com>
2023-12-07 15:23:30 +0700

    fixed index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 4000;
 const expressHbs = require("express-handlebars");
+const path = require('path');
 
 const cookieParser = require("cookie-parser")
 const session = require("express-session")
